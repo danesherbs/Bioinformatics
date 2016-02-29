8d6ab583739cd5a5db609adce82c243cd24ec324
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2016-02-29 23:08:12 +0000

    Add distance_matrix and plot

--- k_medoids.py
+++ k_medoids.py
@@ -1,5 +1,32 @@
+import math
+import matplotlib.pyplot as plt
+
 def k_medoids(data, k):
     pass
+
+def distance_matrix(data):
+    matrix = []
+    for entry in data:
+        entry_coords = entry[1:-1]
+        entry_dists = []
+        for other_entry in data:
+            other_entry_coords = other_entry[1:-1]
+            distance_between = _distance(entry_coords, other_entry_coords)
+            entry_dists.append(round(distance_between, 3))
+        matrix.append(entry_dists)
+    return matrix
+
+def _distance(point1, point2):
+    dist = 0
+    for i in range(len(point1)):
+        dist += ( point1[i] - point2[i] ) ** 2
+        return math.sqrt(dist)
+
+def plot(data):
+    xs = [line[1] for line in data]
+    ys = [line[2] for line in data]
+    plt.plot(xs, ys, '+')
+    plt.show()
 
 if __name__ == '__main__':
     data = [
@@ -14,5 +41,6 @@ if __name__ == '__main__':
         ['Gene9',  0.7,  0.7, 'C1'],
         ['Gene10', 4.9,  0.4, 'C3']
     ]
-    print data
-    # print k_medoids(data, 3)
+
+    # plot(data)
+    print distance_matrix(data)
