8d7807e8dfa3b78838805033a9cfa47e76d88fd1
dgs13 <dgs13@ic.ac.uk>
2016-02-29 15:56:55 +0000

    Add data from coursework

---
+++ k_medoids.py
@@ -0,0 +1,18 @@
+def k_medoids(data, k):
+    pass
+
+if __name__ == '__main__':
+    data = [
+        ['Gene1',  1.1,  1.9, 'C1'],
+        ['Gene2',  2.5,  3.8, 'C2'],
+        ['Gene3',  2.45, 2.4, 'C3'],
+        ['Gene4',  1.8,  4.8, 'C2'],
+        ['Gene5',  2.3,  3.7, 'C2'],
+        ['Gene6',  4.8,  0.8, 'C3'],
+        ['Gene7',  1.9,  1.1, 'C1'],
+        ['Gene8',  1.2,  0.6, 'C1'],
+        ['Gene9',  0.7,  0.7, 'C1'],
+        ['Gene10', 4.9,  0.4, 'C3']
+    ]
+    print data
+    # print k_medoids(data, 3)
