349a9d48ede215f0e663d5c02a424d5a0b49571e
Dane Sherburn <dane.sherburn13@imperial.ac.uk>
2016-03-07 12:23:53 +0000

    Add stuff to main

--- k_medoids.py
+++ k_medoids.py
@@ -1,17 +1,26 @@
 import math
+import random
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 
-def k_medoids(points, k, num_iter=3):
-    centres = [ (1.1, 1.9), (2.5, 3.8), (2.45, 2.4) ]
-    for _ in xrange(num_iter):
+def k_medoids(points, k, centres=None, max_iter=3):
+    if not centres:  # if initial centres not given, pick random ones
+        centres = _pick_k_random_elements(points, k)
+    for itr in xrange(max_iter):
         clusters = _form_clusters(points, centres)
-        clusters = _update_clusters_k_medoids(clusters)
-        print 'clusters', clusters
-    return clusters
+        print 'iteration {}: clusters ='.format(itr), sorted(clusters.keys())
+        clusters = _reassign_cluster_centres(clusters)
+        centres  = clusters.keys()
+    return sorted(clusters.keys())
 
-def _update_clusters_k_medoids(clusters):
+def _pick_k_random_elements(elements, k):
+    rand_elems = []
+    for rand_index in random.sample(xrange(len(elements)), k):
+        rand_elems.append(elements[rand_index])
+    return rand_elems
+
+def _reassign_cluster_centres(clusters):
     new_clusters = {}
     for centre in clusters.keys():
         points = clusters[centre]  # points in cluster
@@ -30,11 +39,11 @@ def _find_centre(points):
     return centre
 
 def _form_clusters(points, centres):
-    # points  = [ (1.3, 4.2), (3.1, 5.2), (4.2, 5.3) ]
-    # centres = [ (1, 1),     (2, 2) ]
+    # points  = [ (1, 1), (3, 3), (8, 8) ]
+    # centres = [ (1, 1), (10, 10) ]
     clusters = {}
     for centre in centres:
-        clusters[centre] = []
+        clusters[centre] = []  # no points assigned to centres yet
     for point in points:
         min_dist = float('inf')
         for centre in centres:
@@ -43,7 +52,7 @@ def _form_clusters(points, centres):
                 min_dist = cur_dist
                 assigned_centre = centre
         clusters[assigned_centre].append(point)  # add point to closest cluster
-    return clusters  # {(1, 1): [(1.3, 4.2)], (2, 2): [(3.1, 5.2), (4.2, 5.3)]}
+    return clusters  # {(1, 1): [(1, 1), (3, 3)], (10,10): [(8, 8)]}
 
 def distance_matrix(points):
     matrix = []
@@ -59,7 +68,7 @@ def _distance(point1, point2):
     dist = 0
     for i in range(len(point1)):
         dist += ( point1[i] - point2[i] ) ** 2
-        return math.sqrt(dist)
+    return math.sqrt(dist)
 
 def plot_data(data):
     xs = [line[1] for line in data]
@@ -67,30 +76,40 @@ def plot_data(data):
     plt.plot(xs, ys, '+')
     plt.show()
 
-# def plot_clusters(clusters):
-#
+def _print_matrix(matrix):
+    for row in matrix:
+        print '\t'.join([str(x) for x in row])
 
 
 if __name__ == '__main__':
+
     data = [
-        ['Gene1',  1.1,  1.9, 'C1'],
-        ['Gene2',  2.5,  3.8, 'C2'],
-        ['Gene3',  2.45, 2.4, 'C3'],
-        ['Gene4',  1.8,  4.8, 'C2'],
-        ['Gene5',  2.3,  3.7, 'C2'],
-        ['Gene6',  4.8,  0.8, 'C3'],
-        ['Gene7',  1.9,  1.1, 'C1'],
-        ['Gene8',  1.2,  0.6, 'C1'],
-        ['Gene9',  0.7,  0.7, 'C1'],
-        ['Gene10', 4.9,  0.4, 'C3']
+    ['Gene1',  1.1,  1.9, 'C1'],
+    ['Gene2',  2.5,  3.8, 'C2'],
+    ['Gene3',  2.45, 2.4, 'C3'],
+    ['Gene4',  1.8,  4.8, 'C2'],
+    ['Gene5',  2.3,  3.7, 'C2'],
+    ['Gene6',  4.8,  0.8, 'C3'],
+    ['Gene7',  1.9,  1.1, 'C1'],
+    ['Gene8',  1.2,  0.6, 'C1'],
+    ['Gene9',  0.7,  0.7, 'C1'],
+    ['Gene10', 4.9,  0.4, 'C3']
     ]
 
-    points = [ (line[1], line[2]) for line in data ]
+    points =  [ (line[1], line[2]) for line in data     ]
+    centres = [ (line[1], line[2]) for line in data[:3] ]
 
-    # points  = [ (1.3, 4.2), (3.1, 5.2), (4.2, 5.3) ]
-    # centres = [ (1, 1),     (2, 2) ]
-    # print _form_clusters(points, centres)
+    print 'DISTANCE BETWEEN GENES:'
+    print  # new line
+    _print_matrix(distance_matrix(points))
 
-    print k_medoids(points, 3)
+    print  # new line
+    print  # new line
 
-    # plot_data(data)
+    print 'PLOTTING DATA...'
+    plot_data(data)
+
+    print  # new line
+    print  # new line
+
+    print 'final cluster = {}'.format(k_medoids(points, 3, centres=centres))
